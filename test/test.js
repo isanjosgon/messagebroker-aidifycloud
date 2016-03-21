ca729b0d5da41c19eaf692947f98fab36ff3f09e
Isra San Jose Gonzalez <isanjosgon@gmail.com>
2016-03-21 21:48:30 +0100

    Add RedisToGo client to test

--- test/test.js
+++ test/test.js
@@ -1,6 +1,13 @@
 'use strict'
 
 const redis = require('redis');
-let client = redis.createClient('9420','telescopefish.redistogo.com');
-client.auth('ca251e430e13bc22a3bd0f228977ee9c');
+let client;
+if (process.env.REDISTOGO_URL) {
+  let rtg = require('url').parse(process.env.REDISTOGO_URL);
+  client = redis.createClient(rtg.port,rtg.hostname);
+  client.auth(rtg.auth.split(':')[1]);
+} else {
+  client = redis.createClient('9420','telescopefish.redistogo.com');
+  client.auth('ca251e430e13bc22a3bd0f228977ee9c');
+}
 client.publish('SERVICE:APIGATEWAY','test');
