30aac20d00b8c92fa9b0b0207cf1ae798bcff51b
Isra San Jose Gonzalez <isanjosgon@gmail.com>
2016-03-21 21:44:01 +0100

    Test message, just to test channel connection

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 'use strict'
 
 const redis = require('redis');
-let client = redis.createClient();
-client.publish('SERVICE:APIGATEWAY','start');
+let client = redis.createClient('9420','telescopefish.redistogo.com');
+client.auth('ca251e430e13bc22a3bd0f228977ee9c');
+client.publish('SERVICE:APIGATEWAY','test');
